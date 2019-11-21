0593244a9caf33542c35e057d922ee954e044104
Julian Prasetyo <picobug.jp@gmail.com>
2019-11-21 08:47:18 +0700

    Initial commit

---
+++ app/User.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App;
+
+use Ramsey\Uuid\Uuid;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+    public $incrementing = false;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $model->uuid = (string) Uuid::uuid4()->toString();
+        });
+    }
+}
